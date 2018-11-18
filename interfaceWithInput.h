2f61a27a1ccccb424404762bb11e446364a66aa3
ia <rigby@wanari.com>
2018-11-18 13:11:12 +0100

    started working with inputs

---
+++ interfaceWithInput.h
@@ -0,0 +1,14 @@
+#ifndef InterfaceWithInput_H
+#define InterfaceWithInput_H
+
+class InterfaceWithInput {
+public:
+    int function1(int a);
+    int function2(int b);
+    int function3(int c);
+    int function5(int e);
+private:
+    int function4(int d);
+};
+
+#endif
