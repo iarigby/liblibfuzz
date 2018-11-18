1526febb29836d9878371180f274600348eb3251
ia <rigby@wanari.com>
2018-11-18 13:16:27 +0100

    ran formatter

--- interfaceWithInput.cpp
+++ interfaceWithInput.cpp
@@ -3,24 +3,22 @@
 int InterfaceWithInput::function1(int a) { return a; }
 int InterfaceWithInput::function2(int b) { return function3(b); }
 int InterfaceWithInput::function3(int c) {
-        if (c > 15) {
-            return function2(c);
-        } else if (c > 3) {
-            int a = function4(c);
-            if (a < 5) {
-                return function3(-5);                
-            }
-        } else {
-            return 3;
-        }
+  if (c > 15) {
+    return function2(c);
+  } else if (c > 3) {
+    int a = function4(c);
+    if (a < 5) {
+      return function3(-5);
     }
+  } else {
+    return 3;
+  }
+}
 int InterfaceWithInput::function5(int e) {
-        if (e % 3 == 0) {
-            return function4(e);
-        } else {
-            return 3;
-        }
-    }
-int InterfaceWithInput::function4(int d) {
-        return d * 5;
-    }
+  if (e % 3 == 0) {
+    return function4(e);
+  } else {
+    return 3;
+  }
+}
+int InterfaceWithInput::function4(int d) { return d * 5; }
--- interfaceWithInput.h
+++ interfaceWithInput.h
@@ -3,12 +3,13 @@
 
 class InterfaceWithInput {
 public:
-    int function1(int a);
-    int function2(int b);
-    int function3(int c);
-    int function5(int e);
+  int function1(int a);
+  int function2(int b);
+  int function3(int c);
+  int function5(int e);
+
 private:
-    int function4(int d);
+  int function4(int d);
 };
 
 #endif
