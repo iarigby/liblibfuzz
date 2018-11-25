b23f0d782267377f45317d8b484aefa7fc015de2
ia <marvinzem@gmail.com>
2018-11-25 15:40:27 +0100

    renamed header file, updated function that had near-impossible condition

--- interfaceWithInput.cpp
+++ interfaceWithInput.cpp
@@ -1,4 +1,5 @@
 #include "interfaceWithInput.h"
+#include "iostream"
 
 int InterfaceWithInput::function1(int a) { return a; }
 int InterfaceWithInput::function2(int b) { return function3(b); }
@@ -6,9 +7,12 @@ int InterfaceWithInput::function3(int c) {
   int res = 3;
   if (c > 15) {
     res = function2(c);
+    std::cout << "if" << std::endl; 
   } else if (c > 3) {
+    std::cout << "else if";
     int a = function4(c);
-    if (a < 5) {
+    if (a < 100) {
+      std::cout << "else if if" << std::endl;
       res = function3(-5);
     }
   }
