403c60e209148eb781d19144d0a0fbd93b73dcf8
iarigby <marvinzem@gmail.com>
2019-02-25 22:54:27 +0100

    using combinationGenerator class in test, removed previous premutation functions which won't be used anymore

--- trace-pc-guard-test.cpp
+++ trace-pc-guard-test.cpp
@@ -3,13 +3,11 @@
 #include "functionPointerMap.cpp"
 #include <string>
 #include "stack.cpp"
+#include "combinationGenerator.cpp"
 
 std::map<std::vector<std::string>, std::vector<std::string>> pcCalls;
 std::vector<std::string> currentPermutation;
 // trace-pc-guard-example.cc
-using permutations = std::vector<std::vector<std::string>>;
-permutations getAllPossibleSequences(std::vector<std::string> v, int maxSize);
-permutations getSubsets(std::vector<std::string> v);
 
 int main(int argc, char **argv) {
 	FunctionPointerMap<stack<int>> a1;
@@ -20,53 +18,21 @@ int main(int argc, char **argv) {
 	a1.insert("isEmpty", &stack<int>::isEmpty);
 	a1.insert("isFull", &stack<int>::isFull);
 	std::vector<std::string> v = {"pop", "peek", "size", "isEmpty", "isFull"};
-	permutations p = getAllPossibleSequences(v, 4);
-	std::cout << "here" << std::endl;
-	for (auto const perm : p) {
+	CombinationGenerator<std::string> cb(v, 3);
+	while (!cb.isDone()) {
+		auto perm = cb.nextCombination();
 		currentPermutation = perm;
 		stack<int> s(5);
-		// s.push(1);
-		// s.push(2);
-		// s.push(3);
+		s.push(1);
+		s.push(2);
+		s.push(3);
 		std::cout << "***** perm";
 		for (auto const elem : perm) 
 			std::cout << " " << elem;
 		std::cout << "\n";
 		for (auto const elem : perm) {
-			// a1.searchAndCall<void>(s, elem);	
+			a1.searchAndCall<void>(s, elem, 3);	
 		}
 		std::cout << std::endl;
 	}
-}
-
-// ATTRIBUTE_NO_SANITIZE_ADDRESS
-permutations getSubsets(std::vector<std::string> v) {
-	permutations allSubsets;
-	for (auto const iter : v) {
-		std::vector<std::string> oneElemSet;
-		oneElemSet.push_back(iter);
-		allSubsets.push_back(oneElemSet);
-		for (auto const setsIter : allSubsets) {
-			if (!(setsIter.size() == 1 && setsIter[0] == iter)) {
-				std::vector<std::string> set;
-				//TODO maybe not necessary to copy one by one
-				for (auto const setElem : setsIter) {
-					set.push_back(setElem);
-				}
-				set.push_back(iter);
-				allSubsets.push_back(set);
-			}			
-		}
-	}
-	return allSubsets;
-}
-
-// ATTRIBUTE_NO_SANITIZE_ADDRESS
-permutations getAllPossibleSequences(std::vector<std::string> v, int maxSize) {
-	permutations p; 
-	// get all subsets
-	permutations allSubsets = getSubsets(v);
-	// include all duplicate elements
-	// permute all subsets
-	return allSubsets;
 }
