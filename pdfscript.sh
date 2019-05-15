203435091cf132ded8ee027ab986f7c3b315bb19
iarigby@ericsson <marvinzem@gmail.com>
2019-05-15 12:08:31 +0200

    byebye

---
+++ pdfscript.sh
@@ -0,0 +1,7 @@
+
+
+
+pdftk thesis.pdf cat 2-32 output thesis1.pdf
+pdftk thesis.pdf cat 33 output thesis3.pdf
+pdftk myrefman.pdf cat 2-11 output thesis2.pdf
+pdftk thesis1.pdf thesis2.pdf thesis3.pdf cat output thesis-final.pdf
