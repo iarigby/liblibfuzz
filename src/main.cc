d2fdcfc777190c7a97c420bd853372603e500843
iarigby@ericsson <marvinzem@gmail.com>
2019-06-17 14:49:09 +0200

    changed stack length for demo

--- src/main.cc
+++ src/main.cc
@@ -29,7 +29,7 @@ int main(int argc, char **argv) {
   // Function that does all the initialization
   InstanceFunctionPointer<stack<int>> getStackInstance =
       [](int sequenceLength) {
-        stack<int> s(sequenceLength);
+        stack<int> s(3);
         return s;
       };
   // Function pointers
